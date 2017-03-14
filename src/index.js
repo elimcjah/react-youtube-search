3936095d361477c08cc1d92bc64283f6bf1cbc5a
Elijah McClendon <Elijah@fungry.com>
2017-03-14 04:49:35 -0600

    Finished the Youtube React Single Page Website.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,53 @@
  * Created by EliMcJah on 3/13/17.
  */
 
-import React from 'react';
-import ReactDom from 'react-dom';
-import SearchBar from './components/search_bar.js';
+import _                    from 'lodash';
+import React, { Component } from 'react';
+import ReactDom             from 'react-dom';
+import YTSearch             from 'youtube-api-search';
+import SearchBar            from './components/search_bar';
+import VideoList            from './components/video_list';
+import VideoDetail          from './components/video_detail';
 
-// const API_KEY = 'AIzaSyDUjRuScbQd1syYVICjqxBfxE3kiJZXeBA';
+const API_KEY = 'AIzaSyDUjRuScbQd1syYVICjqxBfxE3kiJZXeBA';
 
-/*Create a new component. This component should produce some HTML*/
+class App extends Component {
+    constructor(props){
+        super(props);
 
-const App = () => {
-    return (<div>
-        <SearchBar />
-    </div>);
-};
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('Future Crunch')
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term }, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render (){
+
+        const videoSearch = _.debounce((term) => this.videoSearch(term), 500);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={ this.state.selectedVideo } />
+                <VideoList
+                    onVideoSelect = { selectedVideo => this.setState({selectedVideo})}
+                    videos ={ this.state.videos } />
+            </div>);
+    }
+
+}
 
 
-/*Take this components generated HTML and put it on the page(in the DOM)*/
 
 ReactDom.render(<App />, document.querySelector('.container'));
