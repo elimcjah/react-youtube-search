b76fb3c5cc76929844270147cb15cb4e657c56fe
Elijah McClendon <Elijah@fungry.com>
2017-03-15 20:07:17 -0600

    Add postinstall, dev, & start to the package.json.  Created a Server.js file for heroku to run server.

---
+++ server.js
@@ -0,0 +1,21 @@
+/**
+ * Created by EliMcJah on 3/15/17.
+ */
+const express = require('express');
+
+const path = require('path');
+
+const port = process.env.PORT  | 8080;
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+
+});
+
+app.listen(port);
+console.log('Server started');
+
